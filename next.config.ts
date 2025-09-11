3d8096a009f61c2b4802ab4bfe4d0bd3ab63dcf7
tansunyj <jiejie0406@sohu.com>
2025-09-11 15:40:57 +0800

    修复缺陷

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
